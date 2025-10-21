d159bdcdce62cbfe752150f7be8fd16a3df21517
manshu777 <79352371+Manshu777@users.noreply.github.com>
2025-10-21 17:13:46 +0530

    chafnf

--- src/app/components/common/index.js
+++ src/app/components/common/index.js
@@ -1,2 +1,2 @@
-export const baseurl=`http://147.93.18.63:8000/api/v1`
-export const storageLink=`http://147.93.18.63:8000/storage/`
+export const baseurl=`http://147.93.18.63:8001/api/v1`
+export const storageLink=`http://147.93.18.63:8001/storage/`
